a6c7e0cca79447b6e434c4095a6d9665a5392a1f
kannanATexcelforte <kannan@excelforte.com>
2023-12-05 16:40:47 +0530

    05.12.2023-eo
    completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import { fromEvent, interval } from "rxjs";
-import { map, concatMap, take, tap } from "rxjs/operators";
+import { map, exhaustMap, take, tap } from "rxjs/operators";
 import { ajax } from "rxjs/ajax";
 
 const button = document.querySelector('#btn');
 const observable = fromEvent(
     button, 'click'
 ).pipe(
-    concatMap(() => {
+    exhaustMap(() => {
         return ajax.getJSON('https://jsonplaceholder.typicode.com/todos/1').pipe(            
             take(5),
             tap({
